9faec0bd3161fe6c856b63e1b82b9dda3dc1b27e
Shashank Dubey <jfriday464@gmail.com>
2024-05-12 11:45:21 +0530

    Fixes

--- R-Programming/q3.r
+++ R-Programming/q3.r
@@ -1,2 +1,10 @@
-# Create a vector of size 10 with 5th and 7th values as missing (store these values as NA). Use
-# the “is.na()” to find locations of missing data.
+# Create a vector of size 10 with missing values at 5th and 7th positions
+my_vector <- c(1, 2, 3, 4, NA, 6, NA, 8, 9, 10)
+
+# Find locations of missing data
+missing_locations <- which(is.na(my_vector))
+
+# Print the vector and locations of missing data
+print(my_vector)
+print("Locations of missing data:")
+print(missing_locations)
